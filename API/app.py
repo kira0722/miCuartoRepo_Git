81f93a26f611833c6cbe85447ecafc09a53b7bc5
kira0722 <samuel_quirama23211@elpoli.edu.co>
2025-03-26 09:57:17 -0500

    tyr

--- API/app.py
+++ API/app.py
@@ -14,3 +14,5 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+# sada
